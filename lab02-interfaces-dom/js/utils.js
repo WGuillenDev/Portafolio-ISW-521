0e9d63c980c80bf517df52b14872ae09fa5f1ceb
WGuillenDev <wilberthguillen.ing@gmail.com>
2026-07-10 20:52:29 -0600

    feat(lab02-interfaces-dom): agregar funciones necesarias y backoff exponencial

---
+++ lab02-interfaces-dom/js/utils.js
@@ -0,0 +1,46 @@
+const UTILS = {
+  esperar(milisegundos) {
+    return new Promise((resolve) => setTimeout(resolve, milisegundos));
+  },
+
+  calcularEspera(numeroIntento) {
+    return CONFIG.REINTENTO.ESPERA_BASE_MS * Math.pow(2, numeroIntento);
+  },
+
+  async ejecutarConBackoff(funcionFetch) {
+    let ultimoError;
+
+    for (let intento = 0; intento < CONFIG.REINTENTO.MAX_INTENTOS; intento++) {
+      try {
+        return await funcionFetch();
+      } catch (error) {
+        ultimoError = error;
+
+        const esUltimoIntento = intento === CONFIG.REINTENTO.MAX_INTENTOS - 1;
+        if (esUltimoIntento) break;
+
+        const tiempoEspera = this.calcularEspera(intento);
+        await this.esperar(tiempoEspera);
+      }
+    }
+
+    throw ultimoError;
+  },
+
+  async ejecutarConCountdown(segundos, callbackPorSegundo) {
+    for (let segundoActual = segundos; segundoActual > 0; segundoActual--) {
+      callbackPorSegundo(segundoActual);
+      await this.esperar(1000);
+    }
+  },
+
+  formatearFecha(fechaString) {
+    const fecha = new Date(fechaString.replace(" ", "T"));
+    return fecha.toLocaleDateString("es-CR", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  },
+};
