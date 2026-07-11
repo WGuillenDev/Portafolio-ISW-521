a1f9963b4d55733097da35615626fe0d3be7e56c
WGuillenDev <wilberthguillen.ing@gmail.com>
2026-07-10 19:04:04 -0600

    refactor(lab01-landing-pasterelia-mana): mover script de configuracion de tema fuera del head

---
+++ clase06/sección#2/index.js
@@ -0,0 +1,44 @@
+/*const numeros = [40, 34, 5, 8, 100, 6];
+console.log(numeros.sort((a, b) => a-b));
+*/
+
+//imperativo
+/*const dobleImp = [];
+for(let i = 0; i < numeros.lenght; i++){
+    dobleImp.push(numeros[i] *2);
+}
+
+//Declarativo
+const dobleDec = numeros.map(n => n * 2);
+console.log(dobleDec);
+*/
+/*
+//Actividad
+const precios = [100, 250,80, 400];
+const caros = [];
+for(let i = 0; i < precios.length; i++){
+    if(precios[i] > 150)
+        caros.push(precios[i]);
+}
+
+//Declarativo
+const mostrarPrecios = precios.filter(n => n > 150);
+console.log(mostrarPrecios);
+*/
+
+//Actividad 
+/*const estudiantes = [
+    {nombre: "Ana", carnet: "2055655"},
+    {nombre: "luis", carnet: "2024543"}
+];
+estudiantes.map(e=> `${e.nombre} (${e.carnet})`.toLocaleUpperCase());
+console.log(estudiantes);
+*/
+
+/*const estudiantes = [
+    {nombre: "Ana", promedio: 85},
+    {nombre: "luis", promedio: 67},
+    {nombre: "Sara", promedio: 91}
+];
+ const estudianteNotaMayor = estudiantes.filter(e=> e >= 80);
+*/
