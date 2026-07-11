bcc5d0a9c23c55808b6415699a1fec8114139263
WGuillenDev <wilberthguillen.ing@gmail.com>
2026-07-11 15:27:59 -0600

    feat(lab02-interfaces-dom): agregar main.js

---
+++ lab02-interfaces-dom/js/main.js
@@ -0,0 +1,33 @@
+const MAIN = {
+
+  async iniciar() {
+    UI.aplicarTema();
+    UI.aplicarIdioma();
+    UI.aplicarFuente();
+    UI.mostrarCargando();
+
+    try {
+      await AUTH.iniciarSesion();
+
+      const resultado = await API.obtenerSedes();
+
+      if (resultado.desdeCache) {
+        UI.mostrarBannerCache();
+      } else {
+        UI.ocultarBannerCache();
+      }
+
+      UI.mostrarSedes(resultado.datos);
+    } catch (error) {
+      if (error.message !== "Sesión expirada") {
+        UI.mostrarErrorPartidos();
+      }
+    }
+
+    EVENTOS.inicializar();
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  MAIN.iniciar();
+});
